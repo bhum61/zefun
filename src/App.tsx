7e7e8c575da40521dbb90d8b5b686d460d7713c9
Hafiz <hafiz.mov@hotmail.com>
2023-11-29 16:30:32 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Song from "./components/song/Song";
+
+import './bootstrap.min.css'
+
+function App() {
+
+  return (
+    <>
+      <Song />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/song/Song.tsx
@@ -0,0 +1,11 @@
+
+
+const Song = () => {
+
+    return <div>
+        <p>Song</p>
+    </div>;
+}
+
+
+export default Song;
